b447cd87198b2fd4c3248e17ab79c93cce4ac99f
Peter Upfold <strategyoracle+github@gmail.com>
2012-05-09 09:13:55 +0100

    Add exclusion to validate '#' URL, fixes #3

--- total-slider/slides_backend.php
+++ total-slider/slides_backend.php
@@ -431,22 +431,31 @@ class Total_Slider_Backend {
 		
 	*/
 	
-		return (bool)preg_match("
-      /^                                                      # Start at the beginning of the text
-      (?:ftp|https?|feed):\/\/                                # Look for ftp, http, https or feed schemes
-      (?:                                                     # Userinfo (optional) which is typically
-        (?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*      # a username or a username and password
-        (?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@          # combination
-      )?
-      (?:
-        (?:[a-z0-9\-\.]|%[0-9a-f]{2})+                        # A domain name or a IPv4 address
-        |(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\])         # or a well formed IPv6 address
-      )
-      (?::[0-9]+)?                                            # Server port number (optional)
-      (?:[\/|\?]
-        (?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})   # The path and query (optional)
-      *)?
-    $/xi", $url);
+		if ($url === '#')
+		{
+			// allow a '#' character only
+			return true;
+		}
+		else {
+	
+			return (bool)preg_match("
+	      /^                                                      # Start at the beginning of the text
+	      (?:ftp|https?|feed):\/\/                                # Look for ftp, http, https or feed schemes
+	      (?:                                                     # Userinfo (optional) which is typically
+	        (?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*      # a username or a username and password
+	        (?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@          # combination
+	      )?
+	      (?:
+	        (?:[a-z0-9\-\.]|%[0-9a-f]{2})+                        # A domain name or a IPv4 address
+	        |(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\])         # or a well formed IPv6 address
+	      )
+	      (?::[0-9]+)?                                            # Server port number (optional)
+	      (?:[\/|\?]
+	        (?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})   # The path and query (optional)
+	      *)?
+	    $/xi", $url);
+    
+    	}
 	
 	}
 	
