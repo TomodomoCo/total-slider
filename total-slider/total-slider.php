01e85704aea29a0c9d24aacabc7b919f1149587f
Peter Upfold <strategyoracle+github@gmail.com>
2012-06-18 09:35:51 +0100

    Start development on 1.1

--- total-slider/total-slider.php
+++ total-slider/total-slider.php
@@ -3,7 +3,7 @@
 Plugin Name: Total Slider
 Plugin URI: http://www.totalslider.com/
 Description: The best experience for building sliders, with true WYSIWYG, drag & drop and more!
-Version: 1.0.2
+Version: 1.1-alpha
 Author: Peter Upfold
 Author URI: http://www.vanpattenmedia.com/
 License: GPLv2 or later
